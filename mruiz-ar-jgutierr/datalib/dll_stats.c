5174ca64a94740aac6db169489eadb5444705aa7
marruiart <mruiz-ar@student.42malaga.com>
2023-03-02 19:32:05 +0100

    Añadida función dll_empty.c

--- mruiz-ar-jgutierr/datalib/dll_stats.c
+++ mruiz-ar-jgutierr/datalib/dll_stats.c
@@ -3,18 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   dll_stats.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: mruiz-ar <mruiz-ar@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
-/*   Updated: 2023/01/23 11:56:54 by antgalan         ###   ########.fr       */
+/*   Updated: 2023/03/02 19:31:58 by mruiz-ar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "datalib.h"
 
+/**
+ * @brief	Checks if the list is empty.
+ * @param list	Pointer to the first element of the list.
+ *
+ * @return	1 if the list is empty;
+ * 			0 otherwise.
+ */
 int	dll_empty(t_dlist *list)
 {
-	// TODO
+	return (list == NULL);
 }
 
 int	dll_size(t_dlist *list)
