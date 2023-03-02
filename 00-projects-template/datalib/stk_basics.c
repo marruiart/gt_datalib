77aa28ceb37dde3b88d76b557e4ba61ed34fb00a
Juan Manuel Gutierrez Mellado <juanmanuelgutierrezmellado@gmail.com>
2023-03-02 19:56:27 +0100

    Añadida función stk_new

--- 00-projects-template/datalib/stk_basics.c
+++ 00-projects-template/datalib/stk_basics.c
@@ -3,18 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   stk_basics.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: jgutierr <jgutierr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/01 18:40:27 by antgalan          #+#    #+#             */
-/*   Updated: 2023/01/23 12:08:46 by antgalan         ###   ########.fr       */
+/*   Updated: 2023/03/02 19:56:07 by jgutierr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "datalib.h"
 
+/**
+ * @brief	Creates a new element for the stack.
+ *
+ * @param data	The data to store in the element.
+ *
+ * @return	Pointer to the new element.
+ */
 t_stack	*stk_new(void *data)
 {
-	// TODO
+	t_stack	*new;
+
+	new = (t_stack *) malloc(sizeof(t_stack));
+	if (new == NULL)
+		return (NULL);
+	new->data = data;
+	new->next = NULL;
+	return (new);
 }
 
 void	stk_push(t_stack **stack, void *data)
