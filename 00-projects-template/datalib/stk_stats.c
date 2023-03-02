9d2217af695d4d5201bdd9f8ca8750025b5b7142
Juan Manuel Gutierrez Mellado <juanmanuelgutierrezmellado@gmail.com>
2023-03-02 19:58:48 +0100

    Añadida función stk_peek

--- 00-projects-template/datalib/stk_stats.c
+++ 00-projects-template/datalib/stk_stats.c
@@ -6,7 +6,7 @@
 /*   By: jgutierr <jgutierr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/01 18:40:27 by antgalan          #+#    #+#             */
-/*   Updated: 2023/03/02 19:58:23 by jgutierr         ###   ########.fr       */
+/*   Updated: 2023/03/02 19:58:41 by jgutierr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,17 @@ t_stack	*stk_top(t_stack *stack)
 	return (stack);
 }
 
+/**
+ * @brief	Checks the top element data of the stack.
+ *
+ * @param stack	Pointer to the stack.
+ *
+ * @return	Pointer to the top element data;
+ * 			NULL if the stack is empty.
+ */
 void	*stk_peek(t_stack *stack)
 {
-	// TODO
+	if (stk_empty(stack))
+		return (NULL);
+	return (stack->data);
 }
