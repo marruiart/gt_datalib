54f71e51de00d1316327c4437386d164d621f131
Juan Manuel Gutierrez Mellado <juanmanuelgutierrezmellado@gmail.com>
2023-03-02 19:57:47 +0100

    Añadida función stk_empty

--- 00-projects-template/datalib/stk_stats.c
+++ 00-projects-template/datalib/stk_stats.c
@@ -3,18 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   stk_stats.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: jgutierr <jgutierr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/01 18:40:27 by antgalan          #+#    #+#             */
-/*   Updated: 2023/01/23 12:09:21 by antgalan         ###   ########.fr       */
+/*   Updated: 2023/03/02 19:57:38 by jgutierr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "datalib.h"
 
+/**
+ * @brief	Checks if the stack is empty.
+ *
+ * @param stack	Pointer to the stack.
+ *
+ * @return	1 if the stack is empty;
+ * 			0 otherwise.
+ */
 int	stk_empty(t_stack *stack)
 {
-	// TODO
+	return (stack == NULL);
 }
 
 int	stk_size(t_stack *stack)
