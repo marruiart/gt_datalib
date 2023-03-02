3e8dcba9bf7ee95699ac0630d5bbd26423347ffa
marruiart <mruiz-ar@student.42malaga.com>
2023-03-02 19:32:23 +0100

    Añadida función dll_size.c

--- mruiz-ar-jgutierr/datalib/dll_stats.c
+++ mruiz-ar-jgutierr/datalib/dll_stats.c
@@ -6,7 +6,7 @@
 /*   By: mruiz-ar <mruiz-ar@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 12:53:46 by antgalan          #+#    #+#             */
-/*   Updated: 2023/03/02 19:31:58 by mruiz-ar         ###   ########.fr       */
+/*   Updated: 2023/03/02 19:32:16 by mruiz-ar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,24 @@ int	dll_empty(t_dlist *list)
 	return (list == NULL);
 }
 
+/**
+ * @brief   Counts the number of nodes in the list.
+ *
+ * @param list   Pointer to the first element of the list.
+ *
+ * @return  Number of nodes in the list.
+ */
 int	dll_size(t_dlist *list)
 {
-	// TODO
+	int	i;
+
+	i = 0;
+	while (list)
+	{
+		list = list->next;
+		i++;
+	}
+	return (i);
 }
 
 t_dlist	*dll_first(t_dlist *list)
