aedd9e7f1fa8595c4736380b5b978041669876de
Juan Manuel Gutierrez Mellado <juanmanuelgutierrezmellado@gmail.com>
2023-03-02 19:58:11 +0100

    Añadida función stk_size

--- 00-projects-template/datalib/stk_stats.c
+++ 00-projects-template/datalib/stk_stats.c
@@ -6,7 +6,7 @@
 /*   By: jgutierr <jgutierr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/01 18:40:27 by antgalan          #+#    #+#             */
-/*   Updated: 2023/03/02 19:57:38 by jgutierr         ###   ########.fr       */
+/*   Updated: 2023/03/02 19:58:03 by jgutierr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,24 @@ int	stk_empty(t_stack *stack)
 	return (stack == NULL);
 }
 
+/**
+ * @brief	Counts the number of elements in the stack.
+ *
+ * @param stack	Pointer to the stack.
+ *
+ * @return	Number of elements in the stack.
+ */
 int	stk_size(t_stack *stack)
 {
-	// TODO
+	int	size;
+
+	size = 0;
+	while (stack)
+	{
+		size++;
+		stack = stack->next;
+	}
+	return (size);
 }
 
 t_stack	*stk_top(t_stack *stack)
